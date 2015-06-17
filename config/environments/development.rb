18e887a0506123d9d76b7951601d817b8f8f3de3
PRABHALIKA <Prabhalika999@gmail.com>
2015-06-17 18:58:28 +0530

    changed second commit *dfgffd *dfgg
    changed third commit
    *dfgdfg
    *dsfgdfg
    
     fourth commit
    *dsfgdfg
    *dghfhng

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
   config.cache_classes = false
 
   # Do not eager load code on boot.
-  #config.eager_load = false
+  config.eager_load = false
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
@@ -29,7 +29,7 @@ Rails.application.configure do
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
-  config.assets.digest = true
+  #config.assets.digest = true
 
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
